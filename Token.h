eb8539ce0c7f93836fed420aad6f8b7094e96594
Victor Eduardo Reyes <victorrey390@gmail.com>
2026-03-26 19:10:14 -0600

    Modificaciones por hacer
    Cositas que tengo que modificar mas de rato

--- Token.h
+++ Token.h
@@ -2,83 +2,85 @@
 #include <string>
 
 enum TokenType
+
+    //ACABAR EL LEXICO
 {
     // tipos de dato estándar
-    TK_INT,
-    TK_STRING,
-    TK_FLOAT,
-    TK_BOOL,
-    TK_DOUBLE,
-    TK_CHAR,
-    TK_LONG,
-    TK_VOID,
+    TK_INT,    //Quitar
+    TK_STRING,  ///// Dejar
+    TK_FLOAT,    //Quitar
+    TK_BOOL,     //Quitar
+    TK_DOUBLE,    ///// Dejar
+    TK_CHAR,     //Quitar
+    TK_LONG,     //Quitar
+    TK_VOID,    //Quitar
 
-    // tipos de dato personalizados
-    TK_ENTERO,
-    TK_CADENA,
-    TK_DECIMAL,
+    // tipos de dato personalizados   
+    TK_ENTERO,//Quitar
+    TK_CADENA,//Quitar
+    TK_DECIMAL,//Quitar
 
     // estructuras de control
-    TK_IF,
-    TK_ELSE,
-    TK_FOR,
-    TK_WHILE,
+    TK_IF,  //dejar
+    TK_ELSE, //dejar
+    TK_FOR, //dejar
+    TK_WHILE,    /////Quitar este 
 
-    // palabras reservadas creadas por ti
-    TK_INICIO,
-    TK_MOSTRAR,
-    TK_LEER,
-    TK_PRINT,
+    // palabras reservadas creadas por ti  
+    TK_INICIO,    ///Dejar
+    TK_MOSTRAR,///Dejar
+    TK_LEER,///Dejar
+    TK_PRINT,///Dejar
 
-    // identificadores y literales
-    TK_IDENTIFIER,
-    TK_NUMBER,
-    TK_STRING_LITERAL,
-    TK_CHAR_LITERAL,
+    // identificadores y literales  
+    TK_IDENTIFIER,//Quitar
+    TK_NUMBER,      //Quitar
+    TK_STRING_LITERAL,//Quitar
+    TK_CHAR_LITERAL,//Quitar
 
-    // operadores aritméticos
-    TK_PLUS,
-    TK_MINUS,
-    TK_MULT,
-    TK_DIV,
-    TK_MOD,
+    // operadores aritméticos  
+    TK_PLUS,///Dejar
+    TK_MINUS,///Dejar
+    TK_MULT,///Dejar
+    TK_DIV,///Dejar
+    TK_MOD,///Dejar
 
-    // operadores relacionales
-    TK_LESS,
-    TK_GREATER,
-    TK_LESS_EQUAL,
-    TK_GREATER_EQUAL,
+    // operadores relacionales   
+    TK_LESS,///Dejar
+    TK_GREATER,///Dejar
+    TK_LESS_EQUAL,///Dejar
+    TK_GREATER_EQUAL,///Dejar
 
-    // operadores de igualdad
-    TK_EQUAL_EQUAL,
-    TK_NOT_EQUAL,
+    // operadores de igualdad  
+    TK_EQUAL_EQUAL,///Dejar
+    TK_NOT_EQUAL,///Dejar
 
-    // asignación
-    TK_ASSIGN,
+    // asignación   
+    TK_ASSIGN,//Quitar
 
-    // separadores
-    TK_SEMICOLON,
-    TK_COMMA,
+    // separadores   
+    TK_SEMICOLON,//Quitar
+    TK_COMMA,//Quitar
 
-    // agrupadores
-    TK_LPAREN,
-    TK_RPAREN,
-    TK_LBRACE,
-    TK_RBRACE,
+    // agrupadores 
+    TK_LPAREN,//Quitar
+    TK_RPAREN,//Quitar
+    TK_LBRACE,//Quitar
+    TK_RBRACE,//Quitar
 
-    // símbolos especiales
-    TK_HASH,
-    TK_DOLLAR,
-    TK_EXCLAMATION,
-    TK_QUESTION,
-    TK_AMPERSAND,
-    TK_AT,
+    // símbolos especiales   
+    TK_HASH,///Dejar
+    TK_DOLLAR,///Dejar
+    TK_EXCLAMATION,///Dejar
+    TK_QUESTION,///Dejar
+    TK_AMPERSAND,///Dejar
+    TK_AT,///Dejar
 
-    // token desconocido
-    TK_UNKNOWN,
+    // token desconocido    
+    TK_UNKNOWN,///Dejar
 
-    // fin de archivo
-    TK_END_OF_FILE
+    // fin de archivo   
+    TK_END_OF_FILE///Dejar
 };
 
 struct Token
@@ -91,18 +93,18 @@ inline std::string tokenTypeToString(TokenType type)
 {
     switch (type)
     {
-    case TK_INT: return "INT";
-    case TK_STRING: return "STRING";
-    case TK_FLOAT: return "FLOAT";
-    case TK_BOOL: return "BOOL";
-    case TK_DOUBLE: return "DOUBLE";
-    case TK_CHAR: return "CHAR";
-    case TK_LONG: return "LONG";
-    case TK_VOID: return "VOID";
+	case TK_INT: return "INT";  //Quitar
+	case TK_STRING: return "STRING"; ///// Dejar
+	case TK_FLOAT: return "FLOAT"; ///Quitar
+	case TK_BOOL: return "BOOL"; //Quitar
+	case TK_DOUBLE: return "DOUBLE"; ///Dejar
+	case TK_CHAR: return "CHAR"; // Quitar
+	case TK_LONG: return "LONG"; //Quitar
+	case TK_VOID: return "VOID"; //Quitar
 
-    case TK_ENTERO: return "ENTERO";
-    case TK_CADENA: return "CADENA";
-    case TK_DECIMAL: return "DECIMAL";
+	case TK_ENTERO: return "ENTERO"; //Quitar
+	case TK_CADENA: return "CADENA"; //Quitar
+	case TK_DECIMAL: return "DECIMAL"; ///Quitar
 
     case TK_IF: return "IF";
     case TK_ELSE: return "ELSE";
